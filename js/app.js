a23b435a34e12cbe89e198a0ff522946f94c347f
justinsaunders <justinleesaunders@gmail.com>
2019-06-20 00:44:41 -0400

    Fixed query using ajax indenting stylesheet with 4 spaces cleaned up clear button style added list style elements and critical flag indicator

--- js/app.js
+++ js/app.js
@@ -7,10 +7,13 @@ var endOfResults = 0;
 var typingTimer;                //timer identifier
 var doneTypingInterval = 1250;  //time in ms, 5 second for example
 var $input = $('#user-input');
+var $resultsContainer = $('#results-container');
 //on keyup, start the countdown
 $input.on('keyup', function () {
     clearTimeout(typingTimer);
-    typingTimer = setTimeout(doneTyping, doneTypingInterval);
+    typingTimer = setTimeout(function(){
+        doneTyping();
+    }, doneTypingInterval);
 });
 //on keydown, clear the countdown
 $input.on('keydown', function () {
@@ -30,9 +33,9 @@ $('button.clear').on('click', function(){
 function runSearch () {
     if(endOfResults === 0){
         offsetValue = 0;
-        $('#results-container').empty();
-        queryNycDoh(offsetValue);
-        $('#results-container, #more-btn').css('display', 'block');
+        $($resultsContainer).empty();
+        searchNyc(offsetValue);
+        $($resultsContainer).css('display', 'block');
         scrollLoad('#results-container');
         appResize();
         setTimeout(function(){
@@ -61,7 +64,7 @@ function scrollLoad(elem){
             resultHeight = $(elem).innerHeight() + 3;
             if ($(elem).scrollTop() + resultHeight >= $(elem)[0].scrollHeight && $('#user-input')[0].value.length !== 0) {
                 offsetValue = offsetValue + 5;
-                queryNycDoh(offsetValue);
+                searchNyc(offsetValue);
             }
         });
     }
@@ -72,66 +75,143 @@ function loadingAnimationToggle(){
         $('#loading').removeClass("active")},
         500);
 }
-function searchNyc(){
-    $.ajax({
-        url: "https://data.cityofnewyork.us/resource/43nn-pn8j.json",
-        type: "GET",
-        data: {
-            "$limit" : 15,
-            "$$app_token" : "CkMdPHTRlkUB2u1ixJnSUW3Ve"
-        }
-    }).done(function(data) {
-        // alert("Retrieved " + data.length + " records from the dataset!");
-        console.log(data);
+
+
+var baseUrl = "https://data.cityofnewyork.us/resource/";
+var endpoint = "43nn-pn8j.json?";
+var appToken = "CkMdPHTRlkUB2u1ixJnSUW3Ve";
+function searchNyc(offset){
+    if(endOfResults === 0) {
+        loadingAnimationToggle();
+        var userInput = $("input").val().toUpperCase();
+        $.ajax({
+            url: baseUrl + endpoint,
+            type: "GET",
+            dataType: "json",
+            data: {
+                "$limit": 5,
+                "$offset": offset,
+                "$$app_token": appToken,
+                "$where": 'upper(dba) like"%' + userInput + '%"',
+                "$order": "camis ASC",
+                "$select": "camis,MAX(inspection_date)",
+                "$group": "camis"
+            }
+            // , success: function( data, status, jqxhr ){
+            //     console.log( "Request received:", data );
+            // },
+            // error: function( jqxhr, status, error ){
+            //     console.log( "Something went wrong!" );
+            // }
+        }).done(function (data) {
+            var arraySize = data.length;
+            if (((arraySize === 0) || (userInput.includes(";")) || (userInput === "")) && offsetValue === 0) {
+                var returnedInfo = $('<div class="info-display">');
+                $($resultsContainer).append(returnedInfo);
+                $($resultsContainer).children('div:last-child').append('<p class="error-message">OUR ROBOTS CANNOT FIND THAT RESTAURANT.</p><p class="error-message">PLEASE DOUBLE-CHECK THE NAME.</p>');
+            } else if ((offsetValue !== 0) && (arraySize === 0)) {
+                offsetValue = 0;
+                endOfResults = 1;
+            } else if (arraySize !== 0) {
+                refineDohResults(data)
+            }
+        });
+    }
+}
+
+function refineDohResults(data) {
+    $(data).each(function() {
+        $.ajax({
+            url: baseUrl + endpoint,
+            type: "GET",
+            dataType: "json",
+            data: {
+                "camis": this.camis,
+                "inspection_date": this.MAX_inspection_date,
+                "$order": "camis ASC",
+                "$select": "camis,grade,dba,building,street,boro,zipcode,violation_description,critical_flag"
+            }
+        }).done(function(data) {
+            buildResults(data);
+        });
     });
 }
 
-function queryNycDoh(offset){
-    if(endOfResults === 0){
-      loadingAnimationToggle();
-      var userInput = $("input").val().toUpperCase();
-      var baseURL = "https://data.cityofnewyork.us/resource/43nn-pn8j.json";
-      var testQueryString = '?$where=upper(dba) like "%25' + userInput + '%25"&$order=camis ASC &$select=camis,MAX(inspection_date)&$group=camis&$limit=5&$offset=' + offset + '&$$app_token=CkMdPHTRlkUB2u1ixJnSUW3Ve';
-      $.getJSON(baseURL + testQueryString, function(data) {
-          var arraySize = data.length;
-          if (((arraySize === 0) || (userInput.includes(";")) || (userInput === "")) && offsetValue === 0) {
-              var returnedInfo = $('<div class="info-display">');
-              $('#results-container').append(returnedInfo);
-              $('#results-container > div:last-child').append('<p class="error-message">OUR ROBOTS CANNOT FIND THAT RESTAURANT.</p><p class="error-message">PLEASE DOUBLE-CHECK THE NAME.</p>');
-          } else if ((offsetValue !== 0) && (arraySize === 0)){
-              offsetValue === 0;
-              endOfResults = 1;
-          } else if (arraySize !== 0){
-              $(data).each(function(){
-                  var refinedQueryString = "?&camis='" + this.camis + "'&inspection_date='" + this.MAX_inspection_date + "'&$order=camis ASC";
-                  $.getJSON(baseURL + refinedQueryString, function(result){
-
-                      if (result[0].grade === undefined || result[0].grade === "Not Yet Graded") {
-                          var certificateCard = "Z";
-                          var certificateTitle = "NYC Sanitation Grade Pending";
-                      } else {
-                          certificateCard = result[0].grade;
-                          certificateTitle = "NYC Sanitation Grade " + certificateCard;
-                      }
-                      var name = '<h2>' + result[0].dba.replaceAll("/", " / ").replaceAll("Â¢", "¢") + '</h2>';
-                      var grade = '<div class="certificate-display"><div class="certificate '+ certificateCard +'" " title="' + certificateTitle + '"></div></div>';
-                      var address = '<p class="address-display">' + result[0].building + ' ' + result[0].street + ', ' + result[0].boro + '</p>';
-                      $('#results-container').append('<div id="'+result[0].camis+ '" class="info-display"></div>');
-                      $('#results-container > div:last-child').append('<div class="details-container"></div>');
-                      $('#results-container > div:last-child > .details-container').append([name, grade, address]).append('<ul>');
-                      $(result).each(function(){
-                          if (this.violation_description !== undefined){
-                              violationClean = this.violation_description.replaceAll(" Âº", "Âº").replaceAll("Âº ", "Âº").replaceAll("Âº", "°").replaceAll("", "'").replaceAll("''''", "'")
-                          } else {violationClean = "No violation description listed"}
-                          var violation = "<li>" + violationClean + "</li>";
-                          $('#results-container > div:last-child > .details-container > ul').append(violation);
-                      });
-                  });
-              });
-          }
-      });
+function buildResults(result){
+    if (result[0].grade === undefined || result[0].grade === "Not Yet Graded") {
+        var certificateCard = "Z";
+        var certificateTitle = "NYC Sanitation Grade Pending";
+    } else {
+        certificateCard = result[0].grade;
+        certificateTitle = "NYC Sanitation Grade " + certificateCard;
     }
+    var name = '<h2>' + result[0].dba.replaceAll("/", " / ").replaceAll("Â¢", "¢") + '</h2>';
+    var grade = '<div class="certificate-display"><div class="certificate '+ certificateCard +'" " title="' + certificateTitle + '"></div></div>';
+    var address = '<p class="address-display">' + result[0].building + ' ' + result[0].street + ', ' + result[0].boro + " " + result[0].zipcode + '</p>';
+    $($resultsContainer).append('<div id="'+result[0].camis+ '" class="info-display"></div>');
+    $($resultsContainer).children('div:last-child').append('<div class="details-container"></div>');
+    $($resultsContainer).children('div:last-child').children('.details-container').append([name, grade, address]).append('<ul>');
+    $(result).each(function(){
+        if (this.violation_description !== undefined){
+            violationClean = this.violation_description.replaceAll(" Âº", "Âº").replaceAll("Âº ", "Âº").replaceAll("Âº", "°").replaceAll("", "'").replaceAll("''''", "'")
+        } else {violationClean = "No violation description listed"}
+        if (this.critical_flag == "Critical"){
+            var violation = '<li class="critical">' + violationClean + '</li>';
+        } else{
+            var violation = "<li>" + violationClean + "</li>";
+        }
+        $($resultsContainer).children('div:last-child').children('.details-container').children('ul').append(violation);
+    });
 }
+
+// function queryNycDoh(offset){
+//     if(endOfResults === 0){
+//       loadingAnimationToggle();
+//       var userInput = $("input").val().toUpperCase();
+//       var baseURL = "https://data.cityofnewyork.us/resource/";
+//       var endpoint = "43nn-pn8j.json?";
+//       var testQueryString = '$where=upper(dba) like "%25' + userInput + '%25"&$order=camis ASC &$select=camis,MAX(inspection_date)&$group=camis&$limit=5&$offset=' + offset + '&$$app_token=CkMdPHTRlkUB2u1ixJnSUW3Ve';
+//       $.getJSON(baseURL + endpoint + testQueryString, function(data) {
+//           var arraySize = data.length;
+//           if (((arraySize === 0) || (userInput.includes(";")) || (userInput === "")) && offsetValue === 0) {
+//               var returnedInfo = $('<div class="info-display">');
+//               $('#results-container').append(returnedInfo);
+//               $('#results-container > div:last-child').append('<p class="error-message">OUR ROBOTS CANNOT FIND THAT RESTAURANT.</p><p class="error-message">PLEASE DOUBLE-CHECK THE NAME.</p>');
+//           } else if ((offsetValue !== 0) && (arraySize === 0)){
+//               offsetValue = 0;
+//               endOfResults = 1;
+//           } else if (arraySize !== 0){
+//               $(data).each(function(){
+//                   var refinedQueryString = "&camis='" + this.camis + "'&inspection_date='" + this.MAX_inspection_date + "'&$order=camis ASC&$select=grade,dba,building,street,boro,zipcode,violation_description";
+//                   $.getJSON(baseURL + endpoint + refinedQueryString, function(result){
+//                       console.log(result);
+//
+//                       if (result[0].grade === undefined || result[0].grade === "Not Yet Graded") {
+//                           var certificateCard = "Z";
+//                           var certificateTitle = "NYC Sanitation Grade Pending";
+//                       } else {
+//                           certificateCard = result[0].grade;
+//                           certificateTitle = "NYC Sanitation Grade " + certificateCard;
+//                       }
+//                       var name = '<h2>' + result[0].dba.replaceAll("/", " / ").replaceAll("Â¢", "¢") + '</h2>';
+//                       var grade = '<div class="certificate-display"><div class="certificate '+ certificateCard +'" " title="' + certificateTitle + '"></div></div>';
+//                       var address = '<p class="address-display">' + result[0].building + ' ' + result[0].street + ', ' + result[0].boro + " " + result[0].zipcode + '</p>';
+//                       $($resultsContainer).append('<div id="'+result[0].camis+ '" class="info-display"></div>');
+//                       $($resultsContainer).children('div:last-child').append('<div class="details-container"></div>');
+//                       $($resultsContainer).children('div:last-child').children('.details-container').append([name, grade, address]).append('<ul>');
+//                       $(result).each(function(){
+//                           if (this.violation_description !== undefined){
+//                               violationClean = this.violation_description.replaceAll(" Âº", "Âº").replaceAll("Âº ", "Âº").replaceAll("Âº", "°").replaceAll("", "'").replaceAll("''''", "'")
+//                           } else {violationClean = "No violation description listed"}
+//                           var violation = "<li>" + violationClean + "</li>";
+//                           $($resultsContainer).children('div:last-child').children('.details-container').children('ul').append(violation);
+//                       });
+//                   });
+//               });
+//           }
+//       });
+//     }
+// }
 $( document ).ready(function() {
     $( "#user-input" ).focus();
 });
